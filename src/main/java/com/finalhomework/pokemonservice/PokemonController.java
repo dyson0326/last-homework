43a77ea1e9acabdd7299db9fa989c79e03da9102
dyson <dyson0326@docomo.ne.jp>
2024-12-14 09:20:06 +0900

    フロント画面の作成

--- src/main/java/com/finalhomework/pokemonservice/PokemonController.java
+++ src/main/java/com/finalhomework/pokemonservice/PokemonController.java
@@ -1,6 +1,7 @@
 package com.finalhomework.pokemonservice;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -8,18 +9,19 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping("/pokemon")
 public class PokemonController {
 
     private PokemonService pokemonService;
 
     public PokemonController(PokemonService pokemonService) {
-
         this.pokemonService = pokemonService;
     }
 
     //全件検索及び部分検索
-    @GetMapping("/names")
+    @GetMapping("/listnames")
+    @ResponseBody
     public List<Name> getName(@RequestParam(value = "startsWith", required = false) String startsWith) {
         List<Name> names = pokemonService.getName(startsWith);
         return names;
@@ -33,27 +35,28 @@ public class PokemonController {
     }
 
     //入力されたデータの挿入
-    @PostMapping("/names")
-    public ResponseEntity<NameResponse> insert(@RequestBody @Validated NameRequest nameRequest, UriComponentsBuilder uriBuilder) {
+    @PostMapping("/newnames")
+    public ResponseEntity<NameResponse> insert(@ModelAttribute @Validated NameRequest nameRequest, UriComponentsBuilder uriBuilder) {
         Name name = pokemonService.insert(nameRequest.getName(), nameRequest.getType1(), nameRequest.getType2());
         URI location = uriBuilder.path("/names/{id}").buildAndExpand(name.getId()).toUri();
-        NameResponse body = new NameResponse("pokemon created");
+        NameResponse body = new NameResponse("追加しました");
         return ResponseEntity.created(location).body(body);
     }
 
     //更新処理
-    @PatchMapping("/names/{id}")
-    public ResponseEntity<NameResponse> update(@PathVariable("id") Integer id, @RequestBody @Validated NameRequest nameRequest, UriComponentsBuilder uriBuilder) {
+    @PatchMapping("/updatenames/{id}")
+    public ResponseEntity<NameResponse> update(@PathVariable("id") Integer id, @ModelAttribute @Validated NameRequest nameRequest, UriComponentsBuilder uriBuilder) {
         Name name = pokemonService.update(id, nameRequest.getName(), nameRequest.getType1(), nameRequest.getType2());
-        NameResponse body = new NameResponse("pokemon update");
+        NameResponse body = new NameResponse("更新しました");
         return ResponseEntity.ok(body);
     }
 
     //削除処理
-    @DeleteMapping("/names/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+    @DeleteMapping("/deletenames/{id}")
+    public ResponseEntity<NameResponse> delete(@PathVariable("id") Integer id) {
         pokemonService.delete(id);
-        return ResponseEntity.noContent().build();
+        NameResponse body = new NameResponse("削除しました");
+        return ResponseEntity.ok(body);
     }
 
 }
